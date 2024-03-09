bed18eff8cbb96d9e422be7e876d0937227d5d26
Mikhail Orlov <crazyminer398@gmail.com>
2024-03-09 19:04:27 +0100

    feat(Search): add search logic on main page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,21 +1,40 @@
+from django import forms
 from django.shortcuts import render
-from django.http import HttpResponseNotFound
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from . import util
 
+class NewEntryForm(forms.Form):
+    title = forms.CharField(label="Entry's Title")
+    content = forms.CharField(label="New Entry")
+
 
 def index(request):
+    entries = util.list_entries()
+    search = request.GET.get("q")
+
+    if search:
+        if search in entries:
+            return HttpResponseRedirect(reverse("entry", kwargs={"title": search}))
+        entries = [entry for entry in entries if search.lower() in entry.lower()]
+
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": entries
     })
 
 
 def entry(request, title):
     entry = util.get_entry(title)
+
     if entry:
         return render(request, "encyclopedia/entry.html", {
             "title": title,
             "entry": util.get_entry(title)
         })
+    
     return render(request, "encyclopedia/404.html")
 
+
+def add(request):
+    
