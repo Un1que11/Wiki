1f9a3bc188dfd017bc45fe851c3014f9cb28bfc9
Mikhail Orlov <crazyminer398@gmail.com>
2024-03-10 13:53:51 +0100

    feat(Add entry): add create new entry functionality

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,13 +1,15 @@
 from django import forms
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponseBadRequest
 from django.urls import reverse
+
+import os
 
 from . import util
 
 class NewEntryForm(forms.Form):
     title = forms.CharField(label="Entry's Title")
-    content = forms.CharField(label="New Entry")
+    content = forms.CharField(widget=forms.Textarea)
 
 
 def index(request):
@@ -37,4 +39,20 @@ def entry(request, title):
 
 
 def add(request):
-    
+    entries = util.list_entries()
+
+    if request.method == "POST":
+        form = NewEntryForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            
+            if title in entries:
+                return HttpResponseBadRequest("<h1>The entry already exists.</h1>")
+
+            content = form.cleaned_data.get("content")
+            with open(f"C:\\Programming\\CS50 projects\\wiki\\entries\\{title}.md", "w") as f:
+                f.write(content)
+
+    return render(request, "encyclopedia/add.html", {
+        "form": NewEntryForm()
+    })
